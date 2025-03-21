fe4d1196e5639930cd0db0e306d6f16bbdb2bd1f
Dong-Han-Kim <dh32110@gmail.com>
2025-03-21 20:46:51 +0900

    회원가입 기본 요구사항 구현 완료

--- auth.js
+++ auth.js
@@ -1,15 +1,23 @@
 const EMAIL_PATTERN = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
 
+// input
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
+const passwordCheck = document.querySelector('#password_check');
 const nickname = document.querySelector('#nickname');
+
+// alert
 const emailAlert = document.querySelector('.email_alert');
 const passwordAlert = document.querySelector('.password_alert');
+const passwordCheckAlert = document.querySelector('.pw_check_alert');
 const nicknameAlert = document.querySelector('.nickname_alert');
+
+// button
 const authButton = document.querySelector('.auth_button');
 const authBtnLink = document.querySelector('.auth_button a');
 
 let inputStatus;
+let checkPassword = '';
 
 function emailChecker(e) {
 	if (e.target.value === '' && e.target.type === 'email') {
@@ -28,24 +36,6 @@ function emailChecker(e) {
 	}
 }
 
-function passwordChecker(e) {
-	if (e.target.value === '' && e.target.type === 'password') {
-		e.target.classList.add('inputAlert');
-		passwordAlert.classList.add('visible_alert');
-		passwordAlert.textContent = '비밀번호를 입력해주세요.';
-		inputStatus = false;
-	} else if (e.target.value.length < 8) {
-		e.target.classList.add('inputAlert');
-		passwordAlert.classList.add('visible_alert');
-		passwordAlert.textContent = '비밀번호를 8자 이상 입력해주세요.';
-		inputStatus = false;
-	} else {
-		e.target.classList.remove('inputAlert');
-		passwordAlert.classList.remove('visible_alert');
-		inputStatus = true;
-	}
-}
-
 function nicknameChecker(e) {
 	if (e.target.value === '') {
 		e.target.classList.add('inputAlert');
@@ -55,6 +45,39 @@ function nicknameChecker(e) {
 	} else {
 		e.target.classList.remove('inputAlert');
 		nicknameAlert.classList.remove('visible_alert');
+		inputStatus = true;
+	}
+}
+
+function passwordChecker(e) {
+	if (e.target.value === '' && e.target.type === 'password') {
+		e.target.classList.add('inputAlert');
+		passwordAlert.classList.add('visible_alert');
+		passwordAlert.textContent = '비밀번호를 입력해주세요.';
+		inputStatus = false;
+	} else if (e.target.value.length < 8) {
+		console.log(e.target.value.length);
+		e.target.classList.add('inputAlert');
+		passwordAlert.classList.add('visible_alert');
+		passwordAlert.textContent = '비밀번호를 8자 이상 입력해주세요.';
+		inputStatus = false;
+	} else {
+		e.target.classList.remove('inputAlert');
+		passwordAlert.classList.remove('visible_alert');
+		inputStatus = true;
+		checkPassword = e.target.value;
+	}
+}
+
+function passwordDoubleCheck(e) {
+	if (e.target.value !== checkPassword) {
+		e.target.classList.add('inputAlert');
+		passwordCheckAlert.classList.add('visible_alert');
+		passwordCheckAlert.textContent = '비밀번호를 확인해주세요.';
+		inputStatus = false;
+	} else {
+		e.target.classList.remove('inputAlert');
+		passwordCheckAlert.classList.remove('visible_alert');
 		inputStatus = true;
 	}
 }
@@ -69,5 +92,6 @@ function authButtonActivate(e) {
 
 email.addEventListener('focusout', (e) => emailChecker(e));
 password.addEventListener('focusout', (e) => passwordChecker(e));
-nickname.addEventListener('focusout', (e) => nicknameChecker(e));
+passwordCheck?.addEventListener('focusout', (e) => passwordDoubleCheck(e));
+nickname?.addEventListener('focusout', (e) => nicknameChecker(e));
 authBtnLink.addEventListener('click', (e) => authButtonActivate(e));
